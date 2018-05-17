294c6c237f14fcd99ec95e6a27ea8273d47cfb5d
Jacob Evans <jacob@dekz.net>
2018-05-17 15:45:12 +1000

    Update tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import * as Web3 from 'web3';
+import Web3  = require('web3');
 import Web3ProviderEngine = require('web3-provider-engine');
 import RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
-import WebSocketSubprovider = require('web3-provider-engine/subproviders/websocket');
 
 import { InjectedWeb3Subprovider } from '@0xproject/subproviders';
 import { ZeroEx } from '0x.js';
 
 const KOVAN_NETWORK_ID = 42;
 const KOVAN_RPC = 'https://kovan.infura.io';
+declare var window: any;
+declare var document: any;
 
 if ((window as any).web3) {
     // Set up Web3 Provider Engine with a few helper Subproviders from 0x
     const providerEngine = new Web3ProviderEngine();
     providerEngine.addProvider(new InjectedWeb3Subprovider((window as any).web3.currentProvider));
-    providerEngine.addProvider(new RPCSubprovider({ rpcUrl: KOVAN_RPC }));
+    providerEngine.addProvider(new RpcSubprovider({ rpcUrl: KOVAN_RPC }));
     providerEngine.start();
 
     const web3 = new Web3(providerEngine);
