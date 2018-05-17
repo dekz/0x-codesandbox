26ea8a9e8a1fba00f0ebfd00ece990f2b2ce2fae
Jacob Evans <dekz@dekz.net>
2018-05-17 15:06:13 +1000

    Add packages

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import * as Web3 from 'web3';
+import Web3ProviderEngine = require('web3-provider-engine');
+import RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
+import WebSocketSubprovider = require('web3-provider-engine/subproviders/websocket');
+
+import { InjectedWeb3Subprovider } from '@0xproject/subproviders';
+import { ZeroEx } from '0x.js';
+
+const KOVAN_NETWORK_ID = 42;
+const KOVAN_RPC = 'https://kovan.infura.io';
+
+if ((window as any).web3) {
+    // Set up Web3 Provider Engine with a few helper Subproviders from 0x
+    const providerEngine = new Web3ProviderEngine();
+    providerEngine.addProvider(new InjectedWeb3Subprovider((window as any).web3.currentProvider));
+    providerEngine.addProvider(new RPCSubprovider({ rpcUrl: KOVAN_RPC }));
+    providerEngine.start();
+
+    const web3 = new Web3(providerEngine);
+
+    // Initialize 0x.js with the web3 current provider and provide it the network
+    const zeroEx = new ZeroEx(web3.currentProvider, { networkId: KOVAN_NETWORK_ID });
+    console.log(zeroEx);
+}
+
+document.getElementById("app").innerHTML = `
+<h1>Hello 0x.js</h1>
+<div>
+</div>
+`;
